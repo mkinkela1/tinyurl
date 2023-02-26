f6c855b1257b9eb4a3089ebaa6a0ffb70ac3b5fc
Matteo Kinkela <mkinkela123@gmail.com>
2023-02-26 17:56:00 +0100

    Delete FE, I dont like it

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,24 +1,3 @@
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import "bootstrap/dist/css/bootstrap.css";
-import "/styles/globals.css";
-import { useEffect } from "react";
-import SSRProvider from "react-bootstrap/SSRProvider";
-
-export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    typeof document !== undefined
-      ? require("bootstrap/dist/js/bootstrap")
-      : null;
-  }, []);
-  return (
-    <SSRProvider>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="URL shortener app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </SSRProvider>
-  );
+export default function App() {
+  return <></>;
 }
