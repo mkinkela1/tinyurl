6f72fa9da553ea05a38b96e85294890303770753
Matteo Kinkela <mkinkela123@gmail.com>
2023-03-13 22:58:06 +0100

    Add gateway service

---
+++ src/gateway-service/src/main.ts
@@ -0,0 +1,38 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "src/app.module";
+import { Logger } from "@nestjs/common";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { Api } from "src/myApi";
+
+const { api } = new Api();
+api.
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = "api";
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3333;
+  const URL_SERVICE_URL = "http://localhost:8080";
+  const USERS_SERVICE_URL = "http://localhost:8081";
+
+  app.use(
+    ["/api/user", "/api/auth"],
+    createProxyMiddleware({
+      target: USERS_SERVICE_URL,
+      changeOrigin: true
+    })
+  );
+  app.use(
+    ["/api/url", "/api/url-hit"],
+    createProxyMiddleware({
+      target: URL_SERVICE_URL,
+      changeOrigin: true
+    })
+  );
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
